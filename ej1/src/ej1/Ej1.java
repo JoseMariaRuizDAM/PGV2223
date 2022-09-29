bf5cf41971b475282bf64620117759b08a3aa2a3
JoseMariaRuizDAM <91216717+JoseMariaRuizDAM@users.noreply.github.com>
2022-09-29 19:09:21 +0100

    Ejercicios por el profesor

--- ej1/src/ej1/Ej1.java
+++ ej1/src/ej1/Ej1.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ej1;
 
 import java.util.ArrayList;
@@ -18,7 +14,17 @@ public class Ej1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int numero;
+         
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        while(sc.hasNextInt()){ //mientras tenga un numero entero seguira recorriendo el while
+            numeros.add(sc.nextInt());
+        }
+        
+        Collections.sort(numeros); //ordenar la lista
+        System.out.println(numeros);
+        /*
+        int numero = Integer.parseInt(args[0]);
         boolean aux = false;
         ArrayList<Integer> numeros = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in); 
@@ -33,7 +39,7 @@ public class Ej1 {
         Collections.sort(numeros);
         for(Integer i : numeros){
             System.out.print(i + ";");
-        }
+        }*/
 //introducir en una lista los numeros
     }
     
